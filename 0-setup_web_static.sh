6a6a932357ca385c39a30062bcd63446e7371d7f
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-10 05:07:31 +0200

    Create 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Bash script that configures a new Ubuntu machine 
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "Hazem Ghaly" > /data/web_static/releases/test/index.html
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+    add_header X-Served-By \$HOSTNAME;
+    rewrite ^\/redirect_me https://github.com/hazemghaly permanent;
+    root        /etc/nginx/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://github.com/hazemghaly;
+    }
+    error_page 404 /404.html;
+    location = /404.html {
+        root /etc/nginx/html;
+        internal;
+    }
+}
+" | sudo tee /etc/nginx/sites-available/default
+sudo service nginx restart
