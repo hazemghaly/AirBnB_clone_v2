3571b7685cd870fdbf821a86e82e7dd9d07cc38b
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-12 05:20:39 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
 '''
-Fabric script 
+Fabric script
 '''
 
 from fabric.api import put, run, env
 import os
-env.hosts = ['100.27.12.93', '54.146.89.146']
+env.hosts = ["100.27.12.93", "54.146.89.146"]
 
 
 def do_deploy(archive_path):
     ''' an archive to the web servers'''
     if not os.path.isfile(archive_path):
         return False
-    try:
         f = archive_path.split("/")[-1]
         no_ext = f.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, "/tmp/{}".format(f))
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(f, path, no_ext))
-        run('rm /tmp/{}'.format(f))
+    if (put(archive_path, "/tmp/{}".format(f)) and
+        run("mkdir -p {}{}/".format(path, no_ext)) and
+        run("tar -xzf /tmp/{} -C {}{}/".format(f, path, no_ext)) and
+        run("rm /tmp/{}".format(f)) and
         run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(no_ext, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
+            "/data/web_static/releases/{}/".format(no_ext, no_ext)) and
+        run("rm -rf {}{}/web_static".format(path, no_ext)) and
+        run("rm -rf /data/web_static/current") and
+        run("ln -s {}{}/ /data/web_static/current"
+            .format(path, no_ext))).failed is True:
         return False
+
