a6256570746c6b7692b0e5889797be3d3d027cfa
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-12 05:03:40 +0200

    Update 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Fabric script 
+'''
+
+from fabric.api import put, run, env
+import os
+env.hosts = ['100.27.12.93', '54.146.89.146']
+
+
+def do_deploy(archive_path):
+    ''' an archive to the web servers'''
+    if not os.path.isfile(archive_path):
+        return False
+    try:
+        f = archive_path.split("/")[-1]
+        no_ext = f.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/{}".format(f))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(f, path, no_ext))
+        run('rm /tmp/{}'.format(f))
+        run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(no_ext, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
