a15cba780fa8f35cba62f6542808df78d2b6476b
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-01-12 05:40:08 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,14 +15,14 @@ def do_deploy(archive_path):
     f = archive_path.split("/")[-1]
     no_ext = f.split(".")[0]
     path = "/data/web_static/releases/"
-    if (put(archive_path, "/tmp/{}".format(f)) and
-        run("mkdir -p {}{}/".format(path, no_ext)) and
-        run("tar -xzf /tmp/{} -C {}{}/".format(f, path, no_ext)) and
-        run("rm /tmp/{}".format(f)) and
+    if (put(archive_path, "/tmp/{}".format(f)) or
+        run("mkdir -p {}{}/".format(path, no_ext)) or
+        run("tar -xzf /tmp/{} -C {}{}/".format(f, path, no_ext)) or
+        run("rm /tmp/{}".format(f)) or
         run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(no_ext, no_ext)) and
-        run("rm -rf {}{}/web_static".format(path, no_ext)) and
-        run("rm -rf /data/web_static/current") and
+            "/data/web_static/releases/{}/".format(no_ext, no_ext)) or
+        run("rm -rf {}{}/web_static".format(path, no_ext)) or
+        run("rm -rf /data/web_static/current") or
         run("ln -s {}{}/ /data/web_static/current"
             .format(path, no_ext))).failed is True:
         return False
